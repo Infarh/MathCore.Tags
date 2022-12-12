293d79763bf3d810651154f3d5e7168fcc0cf854
Infarh <shmachilin@yandex.ru>
2022-12-12 14:10:02 +0300

    Добавьте файлы проекта.

---
+++ MathCore.Tags/TagExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCore.Tags;
+
+/// <summary>Класс методов-расширений для реализации функциональности добавления объектов, которые могут быть приложены к другим объектам</summary>
+public static class TagExtensions
+{
+    /// <summary>Получить объект-метку указанного типа из целевого объекта</summary>
+    /// <typeparam name="TTag">Тип объекта-метки</typeparam>
+    /// <param name="o">Целевой объект</param>
+    /// <returns>Объект метка, если он существует в указанном объекте</returns>
+    public static TTag? GetTag<TTag>(this object o) => TagPool.Tag<TTag>(o);
+
+    /// <summary>Установить объект-метку для указанного объекта</summary>
+    /// <typeparam name="TTag">Тип объекта-метки</typeparam>
+    /// <param name="o">Целевой объект</param>
+    /// <param name="Tag">Объект-метка, прикладываемый к целевому объекту</param>
+    public static void SetTag<TTag>(this object o, TTag? Tag) => TagPool.SetTag(o, Tag);
+}
---
+++ MathCore.Tags/TagPool.cs
@@ -0,0 +1,101 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MathCore.Tags;
+
+/// <summary>Пул меток</summary>
+internal static class TagPool
+{
+    /// <summary>Объект межпотоковой синхронизации</summary>
+    private static readonly ReaderWriterLockSlim __Lock = new();
+
+    /// <summary>Словарь меток</summary>
+    private static Dictionary<WeakReference, Dictionary<Type, object?>>? __Tags;
+
+    /// <summary>Установить метку указанному объекту</summary>
+    /// <param name="Obj">Целевой объект</param>
+    /// <param name="Tag">Добавляемая метка</param>
+    public static void SetTag(object Obj, object? Tag)
+    {
+        if (Obj is null) throw new ArgumentNullException(nameof(Obj));
+
+        __Lock.EnterWriteLock();
+
+        try
+        {
+            var tags = __Tags ??= new();
+
+            foreach (var w in tags.Keys.Where(w => !w.IsAlive).ToArray())
+                tags.Remove(w);
+
+            //var reference = .Find(Selector);
+
+            WeakReference? reference = null;
+            foreach (var key in tags.Keys)
+                if (ReferenceEquals(Obj, key.Target))
+                {
+                    reference = key;
+                    break;
+                }
+
+            Dictionary<Type, object?> dictionary;
+            if (reference is { })
+                dictionary = tags[reference];
+            else
+                tags.Add(new(Obj), dictionary = new());
+
+            var type = Tag?.GetType() ?? typeof(object);
+
+            if (dictionary.Keys.Contains(type))
+                dictionary[type] = Tag;
+            else
+                dictionary.Add(type, Tag);
+        }
+        finally
+        {
+            __Lock.ExitWriteLock();
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool IsAlive(WeakReference w) => !w.IsAlive;
+
+    /// <summary>Получить метку указанного типа для указанного объекта</summary>
+    /// <typeparam name="TTagType">Тип объекта-метки</typeparam>
+    /// <param name="o">Целевой объект</param>
+    /// <returns>Объект-метка</returns>
+    public static TTagType? Tag<TTagType>(object o)
+    {
+        if (o is null) throw new ArgumentNullException(nameof(o));
+
+        __Lock.EnterReadLock();
+
+        try
+        {
+            var tags = __Tags;
+
+            if (tags is null) return default;
+
+            foreach (var w in tags.Keys)
+                if (w.IsAlive)
+                    tags.Remove(w);
+
+            WeakReference? reference = null;
+            foreach (var key in tags.Keys)
+                if (o.Equals(key.Target))
+                {
+                    reference = key;
+                    break;
+                }
+
+            return reference is null
+                ? default
+                : tags[reference].TryGetValue(typeof(TTagType), out var result)
+                    ? (TTagType?)result
+                    : default;
+        }
+        finally
+        {
+            __Lock.ExitReadLock();
+        }
+    }
+}
